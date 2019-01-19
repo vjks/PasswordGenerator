21167341d4dd1b8af662e5500a54679b87bfb3fe
Vijay Kumar <vj_kumar20@hotmail.com>
2019-01-19 18:06:49 -0500

    Update passgenerator.js

--- passgenerator.js
+++ passgenerator.js
@@ -2,13 +2,14 @@ function main() {
     // Generate the password length
     var minPasswordLength = 15;
     var maxPasswordLength = 30;
+    var numOfUppercase = 2;
     var passLength = randomIntFromInterval( minPasswordLength, maxPasswordLength );
 
     // Create an empty array of that length
     var password = new Array( passLength );
 
     // Add two uppercase letters to the array
-    password = addTwoUppercaseLetters( password );
+    password = addTwoUppercaseLetters( password,  );
 
     // Fill the rest of the array with random characters
     password = fillArray( password );
@@ -48,7 +49,14 @@ function addTwoUppercaseLetters( password, numOfChars ) {
     var alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     for (var i = 0; i < numOfChars; i++) {
         passText += alphabets.charAt(Math.floor(Math.random() * alphabets.length));
-    }    
+        location = randomIntFromInterval( minPasswordLength, maxPasswordLength );
+        if( password[ location ] is not empty ) {
+        	password[ location ] = char;
+        }
+    }
+    // Generate two random numbers between 15 and 30, including 15 and 30. 
+
+    // In a for loop add the chars at the two randomly generated spots. If the spot already exists then add it to the next spot.
     return password;
 }
 
